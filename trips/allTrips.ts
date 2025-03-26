f9101cc82208d00e97382a783741e50b87fc971c
Nathan Wuttke <nathwutt@gmail.com>
2025-03-26 03:34:54 -0500

    Update/fix allTrips

--- trips/allTrips.ts
+++ trips/allTrips.ts
@@ -1,8 +1,10 @@
 import { flinders } from '~/flinders/flindersHike'
-import { germanGigs } from '~/germanGigs/germanGigs'
 import { golden_day_in_sweden } from '~/golden_day_in_sweden/golden_day_in_sweden'
 import { los_angeles } from '~/los_angeles/los_angeles'
 import { texas } from '~/texas/texas'
+import { melbourne } from './melbourne/melbourne'
+import { upstate_new_york } from './upstate_new_york/upstate_new_york'
+import { london } from './london/london'
 
 export interface Trip {
   id: string
@@ -32,4 +34,12 @@ export function getTripHeaderInfoById(id: string) {
   return { locationText, date, headerImage }
 }
 
-export const allTrips: Trip[] = [golden_day_in_sweden, germanGigs, texas, los_angeles, flinders]
+export const allTrips: Trip[] = [
+  upstate_new_york,
+  london,
+  golden_day_in_sweden,
+  texas,
+  melbourne,
+  los_angeles,
+  flinders
+]
