3b712e39f4d4449954d2ec493d0bf3c3e6aa0b65
Nathan Wuttke <nathwutt@gmail.com>
2025-04-28 14:58:29 -0400

    Update; cleanup alltrips.

--- trips/allTrips.ts
+++ trips/allTrips.ts
@@ -7,8 +7,8 @@ import { upstate_new_york } from './upstate_new_york/upstate_new_york'
 import { london } from './london/london'
 import type { Trip } from '@/functions/trips'
 import { detroit } from './detroit/detroit'
-import { tupelo } from './tupelo/tupelo'
-import { minneapolis } from './minneapolis/minneapolis'
+// import { tupelo } from './tupelo/tupelo'
+// import { minneapolis } from './minneapolis/minneapolis'
 
 export const allTrips: Trip[] = [
   // minneapolis,
